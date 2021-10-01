5eb522bf44997493c765c5ac47a9f774510b2e13
Roman Fazizyanov <fazizyanov.exceedteam@gmail.com>
2021-10-01 15:40:58 +0300

    Todo-list frontend

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
-let allTasks = JSON.parse(localStorage.getItem('tasks')) || [];
+let allTasks = [];
 let valueInput = '';
 let input = null;
+let activeEditTask = null
 
-window.onload = function init() {
+window.onload = async function init() {
     input = document.getElementById('add-task')
     input.addEventListener('change', updateValue);
+    const resp = await fetch('http://localhost:8000/allTasks', {
+        method: 'GET'
+    });
+    let result = await resp.json();
+    allTasks = result.data;
     render();
 }
 
-const onClickButton = () => {
-    allTasks.push({
-        text: valueInput,
-        isCheck: false
+const onClickButton = async () => {
+    const resp = await fetch('http://localhost:8000/createTask', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8',
+            'Access-Control-Allow-Origin': '*'
+        },
+        body: JSON.stringify({
+            text: valueInput,
+            isCheck: false
+        })
     });
-    localStorage.setItem('tasks', JSON.stringify(allTasks));
+    let result = await resp.json();
+    console.log(result.data);
+    allTasks = result.data;
     valueInput = '';
     input.value = '';
     render();
@@ -38,7 +53,7 @@ const render = () => {
         checkbox.id = 'checkBoxId';
         checkbox.type = 'checkbox';
         checkbox.checked = item.isCheck;
-        checkbox.onchange = () => onChangeCheckbox(index);
+        checkbox.onchange = () => onChangeCheckbox(index, item._id);
         container.appendChild(checkbox);
 
         const text = document.createElement('p'); // создаю параграф для текста
@@ -49,7 +64,7 @@ const render = () => {
 
         const imageEdit = document.createElement('img');// Edit
         imageEdit.id = 'imageEdit';
-        imageEdit.src = 'edit-list.svg';
+        imageEdit.src = './img/edit-list.svg';
         container.appendChild(imageEdit);
 
         const inputEdit = document.createElement('input'); // новый инпут для исправления
@@ -68,41 +83,38 @@ const render = () => {
             imageDel.classList.toggle('hide');
             inputEdit.classList.toggle('hide');
             imageDone.classList.toggle('hide');
-            localStorage.setItem('tasks', JSON.stringify(allTasks));
         }
         
         const imageDel = document.createElement('img'); // Delete
-        imageDel.src = 'cancel.svg';
+        imageDel.src = './img/cancel.svg';
         imageDel.id = 'imageDelete';
         container.appendChild(imageDel);
-        imageDel.onclick = () => clickDel(index);
+        imageDel.onclick = () => clickDel(item._id);
 
-        const clickDel = (value) => { // Функция Del
-            allTasks = allTasks.filter((item, index) => index !== value)
+        const clickDel = async (value) => { // Функция Del
             console.log('Удалил');
-            localStorage.setItem('tasks', JSON.stringify(allTasks));
+            console.log('value', value)
+            const resp = await fetch(`http://localhost:8000/deleteTask?_id=${value}`, {
+                method: 'DELETE',
+                headers: {
+                'Content-Type': 'application/json;charset=utf-8',
+                'Access-Control-Allow-Origin': '*'
+                }
+            });
+            let result = await resp.json();
+            console.log('result delete', result);
+            allTasks = result.data;
             render();
         }
 
         const imageDone = document.createElement('img'); // Done
-        imageDone.src = 'check-mark.svg';
+        imageDone.src = './img/check-mark.svg';
         imageDone.id = 'imageDone';
         imageDone.classList.add('hide');
         container.appendChild(imageDone);
 
-        imageDone.onclick = () => clickDone(index);
-        const clickDone = (index) => { // Функция Done
-            console.log(allTasks[index].text, inputEdit.value)
-            allTasks[index].text = inputEdit.value;
-            checkbox.classList.toggle('hide');
-            text.classList.toggle('hide');
-            imageEdit.classList.toggle('hide');
-            imageDel.classList.toggle('hide');
-            inputEdit.classList.toggle('hide');
-            imageDone.classList.toggle('hide');
-            localStorage.setItem('tasks', JSON.stringify(allTasks));
-            render()
-        }
+        imageDone.onclick = () => clickDone(item._id, checkbox, text, imageEdit, imageDel, inputEdit, imageDone);
+
         content.appendChild(container);
 
         const btnRemove = document.getElementById('btnRemove');
@@ -113,15 +125,56 @@ const render = () => {
     });
 };
 
-const onClickRemove = () => {
+const onClickRemove = async () => {
+    const resp = await fetch(`http://localhost:8000/deleteAllTask`, {
+        method: 'DELETE',
+        headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+        'Access-Control-Allow-Origin': '*'
+        },
+    });
     console.log('удалил всё')
     allTasks = [];
-    localStorage.removeItem('tasks');
     render();
 }
 
-const onChangeCheckbox = (index) => {  // Функция состояния checkbox
+const onChangeCheckbox = async (index, val) => {  // Функция состояния checkbox
     allTasks[index].isCheck = !allTasks[index].isCheck;
-    localStorage.setItem('tasks', JSON.stringify(allTasks));
+    const resp = await fetch(`http://localhost:8000/updateTask`, {
+        method: 'PATCH',
+        headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+        'Access-Control-Allow-Origin': '*'
+        },
+        body: JSON.stringify({
+            isCheck:  allTasks[index].isCheck,
+            _id: val
+        })
+    });
+    let result = await resp.json();
+    allTasks = result.data;
     render();
+}
+
+const clickDone = async (val, checkbox, text, imageEdit, imageDel, inputEdit, imageDone) => { // Функция Done
+    const resp = await fetch(`http://localhost:8000/updateTask`, {
+        method: 'PATCH',
+        headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+        'Access-Control-Allow-Origin': '*'
+        },
+        body: JSON.stringify({
+            text: inputEdit.value,
+            _id: val
+        })
+    });
+    let result = await resp.json();
+    allTasks = result.data;
+    checkbox.classList.toggle('hide');
+    text.classList.toggle('hide');
+    imageEdit.classList.toggle('hide');
+    imageDel.classList.toggle('hide');
+    inputEdit.classList.toggle('hide');
+    imageDone.classList.toggle('hide');
+    render()
 }
